cff0ba6e085e8acf2177020c4840b6c60c193403
cortezvini97 <cortezvinicius881@gmail.com>
2024-03-06 14:31:21 -0300

    teclado

--- src/C_files/kernel.c
+++ src/C_files/kernel.c
@@ -1,6 +1,25 @@
 #include "screen.h"
+#include "keyboard.h"
+#include "string.h"
 
 void main_kernel(){
     clearScreen();
-    print("Hello World\n");
+    print("Welcome to Cortez OS\n");
+    while (1)
+    {
+        print("\nCORTEZ> ");
+        string ch = readStr();
+        if(strEql(ch,"cmd"))
+        {
+            print("\nYou are allready in cmd\n");
+        }else if(strEql(ch, "clear")){
+            clearScreen();
+        }else if(strEql(ch, "exit")){
+            print("\nbye !");
+            break;
+        }else {
+            print("\nBad command!\n");
+        }
+    }
+    print("\n");
 }
